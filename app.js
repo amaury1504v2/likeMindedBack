0e58cbd04be8744a39dcf50d571198fe44db3f8f
jelaip <jelaiperdudsl@gmail.com>
2021-11-30 14:14:41 +0100

    ini

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const port = 3000
+const userRoutes = require('./routes/user')
+const path = require('path')
+mongoose.connect('',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+})
+.then(()=> console.log("db valid"))
+.catch(()=> console.log("db error"))
+
+app.use((req,res,next)=> {
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content,Accept,Content-Type,Authorization')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE')
+    next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended :true
+}))
+app.use('/user',userRoutes)
+
+module.exports = app
